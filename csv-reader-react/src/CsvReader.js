28e44237284e308c174bcfa0b81db0a3c5fa0625
Esther White <esteewhite@hotmail.com>
2023-01-16 16:52:42 +0200

    displaying data

--- csv-reader-react/src/CsvReader.js
+++ csv-reader-react/src/CsvReader.js
@@ -1,7 +1,27 @@
 import {useState } from 'react'
 
 export default function CsvReader() {
-    const [csvFile, setCSvFile] = useState();
+    const [csvFile, setCsvFile] = useState();
+    const [csvArray, setCsvArray] = useState([]);
+
+    // make an array of objects with values corresponding to the data 
+
+    const processCSV = (str, delim=",") => {
+        const headers = str.slice(0, str.indexOf('\n')).split(delim);
+        
+        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+
+        const newArray = rows.map( row => {
+            const values = row.split(delim);
+            const eachObj = headers.reduce((obj, header, i) => {
+                obj[header] = values[i];
+                return obj;
+            },{})
+            return eachObj;
+        })
+
+        setCsvArray(newArray);
+    }
 
     const submit = () => {
         const file = csvFile;
@@ -10,6 +30,7 @@ export default function CsvReader() {
         reader.onload = function(e) {
             const text = e.target.result;
             console.log(text);
+            processCSV(text);
         }
 
         reader.readAsText(file);
@@ -22,7 +43,7 @@ export default function CsvReader() {
             accept='.csv' 
             id='csv-file'
             onChange={(e) =>{
-                setCSvFile(e.target.files[0])
+                setCsvFile(e.target.files[0])
             }}
             >
             </input>
@@ -36,6 +57,30 @@ export default function CsvReader() {
             >
                 Submit
             </button>
+            {csvArray.length > 0 ? 
+            <>
+                <table>
+                    <thead>
+                        <th>EmpID</th>
+                        <th>ProjectID</th>
+                        <th>DateFrom</th>
+                        <th>DateTo</th>
+                    </thead>
+                    <tbody>
+                        {
+                            csvArray.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.EmpID}</td>
+                                    <td>{item.ProjectID}</td>
+                                    <td>{item.DateFrom}</td>
+                                    <td>{item.DateTo}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </> 
+            : null}
         </form>
     );
 }
