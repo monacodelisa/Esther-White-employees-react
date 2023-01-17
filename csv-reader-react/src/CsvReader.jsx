5a22cfa0fa6a8b7615428b3ac4da34e2ddca3aa3
Esther White <esteewhite@hotmail.com>
2023-01-17 06:27:44 +0200

    starting calculations

--- csv-reader-react/src/CsvReader.jsx
+++ csv-reader-react/src/CsvReader.jsx
@@ -2,6 +2,44 @@ import React, { useState, useRef } from "react";
 import Papa from "papaparse";
 import "./CsvReader.scss";
 
+const findLongestWorkingPair = (tableData) => {
+	let longestPair = { emp1: null, emp2: null, daysWorked: 0 };
+
+	for (let i = 0; i < tableData.length; i++) {
+		for (let j = i + 1; j < tableData.length; j++) {
+			let emp1 = tableData[i];
+			let emp2 = tableData[j];
+
+			if (
+				emp1.EmpID !== emp2.EmpID &&
+				emp1.ProjectID === emp2.ProjectID
+			) {
+				let dateFrom1 = new Date(emp1.DateFrom);
+				let dateTo1 = new Date(emp1.DateTo);
+				let dateFrom2 = new Date(emp2.DateFrom);
+				let dateTo2 = new Date(emp2.DateTo);
+
+				let daysWorked = 0;
+				if (dateFrom1 < dateTo2 && dateFrom2 < dateTo1) {
+					daysWorked =
+						Math.min(dateTo1, dateTo2) -
+						Math.max(dateFrom1, dateFrom2);
+				}
+
+				if (daysWorked > longestPair.daysWorked) {
+					longestPair = {
+						emp1: emp1.EmpID,
+						emp2: emp2.EmpID,
+						daysWorked,
+					};
+				}
+			}
+		}
+	}
+
+	return `${longestPair.emp1}, ${longestPair.emp2}, ${longestPair.daysWorked}`;
+};
+
 const FileUpload = () => {
 	const [file, setFile] = useState(null);
 	const [tableData, setTableData] = useState([]);
@@ -9,17 +47,21 @@ const FileUpload = () => {
 	const fileInputRef = useRef(null);
 	const [isDataLoaded, setIsDataLoaded] = useState(false);
 	const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+	const [longestWorkingPair, setLongestWorkingPair] = useState(null);
 
 	const handleFileUpload = (e) => {
 		let selectedFile = e.target.files[0];
-			if (selectedFile && selectedFile.type === 'text/csv' || selectedFile.name.endsWith('.csv')) {
-				setFile(selectedFile);
-				setFileName(selectedFile.name);
-				setIsButtonDisabled(false);
-			} else {
-				setIsButtonDisabled(true);
-				alert('Please select a valid CSV file');
-			}
+		if (
+			(selectedFile && selectedFile.type === "text/csv") ||
+			selectedFile.name.endsWith(".csv")
+		) {
+			setFile(selectedFile);
+			setFileName(selectedFile.name);
+			setIsButtonDisabled(false);
+		} else {
+			setIsButtonDisabled(true);
+			alert("Please select a valid CSV file");
+		}
 		setFile(e.target.files[0]);
 		setFileName(e.target.files[0].name);
 	};
@@ -50,6 +92,11 @@ const FileUpload = () => {
 		setIsDataLoaded(false);
 	};
 
+	const handleLongestWorkingPair = () => {
+		const result = findLongestWorkingPair(tableData);
+		setLongestWorkingPair(result);
+	};
+
 	return (
 		<div className="container">
 			<input
@@ -57,7 +104,12 @@ const FileUpload = () => {
 				ref={fileInputRef}
 				onChange={(e) => handleFileUpload(e)}
 			/>
-			<button onClick={() => isDataLoaded ? clearTableData(): parseCSVData()} disabled={isButtonDisabled}>
+			<button
+				onClick={() =>
+					isDataLoaded ? clearTableData() : parseCSVData()
+				}
+				disabled={isButtonDisabled}
+			>
 				{isDataLoaded ? "Clear" : "Submit"}
 			</button>
 			{tableData.length === 0 ? (
@@ -83,6 +135,14 @@ const FileUpload = () => {
 						))}
 					</tbody>
 				</table>
+			)}
+			{isDataLoaded && (
+				<>
+					<button onClick={handleLongestWorkingPair}>
+						Worked Together Longest
+					</button>
+					{longestWorkingPair && <p>{longestWorkingPair}</p>}
+				</>
 			)}
 		</div>
 	);
